e76ce3090297de841ce0c34a7e34e4eaf582b33b
phborba <philipeborba@gmail.com>
2021-03-05 14:10:13 -0300

    changing docker flag

--- .docker/start.sh
+++ .docker/start.sh
@@ -4,5 +4,5 @@ docker-compose up -d
 echo 'Wait 10 seconds'
 sleep 10
 echo 'Installation of the plugin'
-docker exec -it deeplearningtools-testing-env sh -c "qgis_setup.sh DeepLearningTools"
+docker exec -t deeplearningtools-testing-env sh -c "qgis_setup.sh DeepLearningTools"
 echo 'Containers are running'
