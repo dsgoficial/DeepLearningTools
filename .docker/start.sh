31a065a785fceba9520dc6dc149afc625a84f014
phborba <philipeborba@gmail.com>
2021-03-05 13:54:10 -0300

    reorganization and new github actions

---
+++ .docker/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker-compose up -d --force-recreate
+echo 'Wait 10 seconds'
+sleep 10
+echo 'Installation of the plugin'
+docker exec -t deeplearningtools-testing-env sh -c "qgis_setup.sh DeepLearningTools"
+echo 'Containers are running'
