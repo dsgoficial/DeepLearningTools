a3e7c305a858abe5f567eed00c75d935478db92e
phborba <philipeborba@gmail.com>
2021-03-05 14:03:16 -0300

    another fix

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker-compose up -d --force-recreate
+docker-compose up -d
 echo 'Wait 10 seconds'
 sleep 10
 echo 'Installation of the plugin'
-docker exec -t deeplearningtools-testing-env sh -c "qgis_setup.sh DeepLearningTools"
+docker exec -it deeplearningtools-testing-env sh -c "qgis_setup.sh DeepLearningTools"
 echo 'Containers are running'
