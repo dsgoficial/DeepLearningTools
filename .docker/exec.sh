7f069317bd87e42941061ad715b9e4dbe716eefd
phborba <philipeborba@gmail.com>
2021-03-08 14:00:59 -0300

    trying again

--- .docker/exec.sh
+++ .docker/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -t deeplearningtools-testing-env sh -c "cd /tests_directory && qgis_testrunner.sh tests.test_image_processing"
+docker exec -t deeplearningtools-testing-env sh -c "cd /github_repo && qgis_testrunner.sh tests.test_image_processing"
